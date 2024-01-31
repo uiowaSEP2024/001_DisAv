d61518db273975081d39300221f6e8432089665e
Joslin Some <joslin-some@uiowa.edu>
2024-01-31 04:13:37 -0600

    ENH: Adding precommit

--- mobile/screens/ForgotPassword.js
+++ mobile/screens/ForgotPassword.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 import { Button, Title } from 'react-native-paper';
 
-export default function Login({ navigation }) {
+export default function ForgotPassword({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
-      <Title>Login Screen</Title>
-      <Button mode="contained" onPress={() => navigation.navigate('SignUp')}>
-        Go to Sign Up
+      <Title>Forgot Password Screen</Title>
+      <Button mode="contained" onPress={() => navigation.navigate('Login')}>
+        Go to Login
       </Button>
     </View>
   );
--- mobile/screens/Rewards.js
+++ mobile/screens/Rewards.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 import { Button, Title } from 'react-native-paper';
 
-export default function Settings({ navigation }) {
+export default function Rewards({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
-      <Title>Settings Screen</Title>
-      <Button mode="contained" onPress={() => navigation.navigate('Home')}>
-        Go to Home
+      <Title>Rewards Screen</Title>
+      <Button mode="contained" onPress={() => navigation.navigate('Settings')}>
+        Go to Settings
       </Button>
     </View>
   );
