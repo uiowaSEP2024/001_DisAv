3e61ee3d6425a8aba14c1544ce7d2f2240b5f893
Joslin Some <joslin-some@uiowa.edu>
2024-02-05 14:11:46 -0600

    ENH: Completed User routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { authRouter } from './routes/AuthRoute.js';
+import { UserRouter } from './routes/UserRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/auth', authRouter);
-
+app.use('/user', UserRouter);
 const dbUri =
   process.env.NODE_ENV === 'test'
     ? 'mongodb+srv://' +
--- web/src/components/__tests__/navbar.test.js
+++ web/src/components/__tests__/navbar.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Navbar from '../Navbar.js';
 
-
 test('renders navbar', () => {
   render(<Navbar />);
 
