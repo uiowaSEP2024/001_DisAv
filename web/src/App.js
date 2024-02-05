6d5a521983377d661b5e32268972c4d4b41e1571
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2024-02-04 22:17:53 -0600

    adding router package creating login components

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './components/Homepage';
-
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
