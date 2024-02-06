6237310bce512fcb75f1fafb6e8089d2ddd075b8
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2024-02-06 11:22:23 -0600

    Issue was with eslint rule, it preffered me using screen.getBy Text instead

--- web/src/components/__tests__/dashboard.test.js
+++ web/src/components/__tests__/dashboard.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Dashboard from '../Dashboard';
 
 test('renders Dashboard with user information', () => {
   const user = { username: 'adnane', firstname: 'Adnane', lastname: 'Ezouhri' };
   sessionStorage.setItem('user', JSON.stringify(user));
 
-  const { getByText } = render(<Dashboard />);
+  render(<Dashboard />);
 
-  // expect(getByText('Welcome to Your Dashboard')).toBeInTheDocument();
-  // expect(getByText(`Username: ${user.username}`)).toBeInTheDocument();
-  // expect(getByText(`First name: ${user.firstname}`)).toBeInTheDocument();
-  // expect(getByText(`Last name: ${user.lastname}`)).toBeInTheDocument();
+  expect(screen.getByText('Welcome to Your Dashboard')).toBeInTheDocument();
+  expect(screen.getByText(`Username: ${user.username}`)).toBeInTheDocument();
+  expect(screen.getByText(`First name: ${user.firstname}`)).toBeInTheDocument();
+  expect(screen.getByText(`Last name: ${user.lastname}`)).toBeInTheDocument();
 });
