c1004ab35f4afd97394437a367a0e71d377ae193
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2024-02-06 11:13:10 -0600

    Able to run the app.test.js no problem, was due to some of the node modules not being imported. Now will attempt to fix login. test.js

--- web/src/components/__tests__/dashboard.test.js
+++ web/src/components/__tests__/dashboard.test.js
@@ -1,15 +1,15 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import Dashboard from '../Dashboard';
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from '../Dashboard';
 
-// test('renders Dashboard with user information', () => {
-//   const user = { username: 'adnane', firstname: 'Adnane', lastname: 'Ezouhri' };
-//   sessionStorage.setItem('user', JSON.stringify(user));
+test('renders Dashboard with user information', () => {
+  const user = { username: 'adnane', firstname: 'Adnane', lastname: 'Ezouhri' };
+  sessionStorage.setItem('user', JSON.stringify(user));
 
-//   const { getByText } = render(<Dashboard />);
+  const { getByText } = render(<Dashboard />);
 
-//   expect(getByText('Welcome to Your Dashboard')).toBeInTheDocument();
-//   expect(getByText(`Username: ${user.username}`)).toBeInTheDocument();
-//   expect(getByText(`First name: ${user.firstname}`)).toBeInTheDocument();
-//   expect(getByText(`Last name: ${user.lastname}`)).toBeInTheDocument();
-// });
+  // expect(getByText('Welcome to Your Dashboard')).toBeInTheDocument();
+  // expect(getByText(`Username: ${user.username}`)).toBeInTheDocument();
+  // expect(getByText(`First name: ${user.firstname}`)).toBeInTheDocument();
+  // expect(getByText(`Last name: ${user.lastname}`)).toBeInTheDocument();
+});
