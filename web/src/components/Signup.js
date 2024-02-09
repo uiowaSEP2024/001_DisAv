930d6a18d51f9272aa13139624dd267ab966c022
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2024-02-09 00:32:55 -0600

    Attempted to add toast as flash messages to enhance sign up page without success.

--- web/src/components/Signup.js
+++ web/src/components/Signup.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/signup.css';
 
+
+
 function Signup() {
+//   toast.configure();
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [firstname, setFirstname] = useState('');
   const [lastname, setLastname] = useState('');
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,7 +28,14 @@ function Signup() {
         email: email
       })
       .then(r => {
-        console.log(r.data.message);
+        if (r.data.message === 'User already exists') {
+            console.log('Username is already used');
+          toast('Username is already used');
+
+        } else {
+          toast('Account has been created successfully');
+          navigate('/'); // Redirect to home page
+        }
       })
       .catch(err => {
         console.log(err);
@@ -30,10 +45,9 @@ function Signup() {
     }
   };
 
-
   return (
     <div className="signup">
-      <form onSubmit={handleSubmit} data-testid="signup-form">
+      <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
         <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
         <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
