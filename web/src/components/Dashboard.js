475221ba36b0d0f8556e5ad6699a78479ca0ae56
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2024-02-18 15:00:10 -0600

    UI is set up, however issues communicating with db

--- web/src/components/Dashboard.js
+++ web/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/dashboard.css';
-
+import Preference from './Preference';
 const Dashboard = () => {
   // Retrieve user information from session storage
   const user = JSON.parse(sessionStorage.getItem('user'));
@@ -26,6 +26,7 @@ const Dashboard = () => {
           {/* Add more user details as needed */}
         </div>
       )}
+      <Preference />
     </div>
   );
 };
