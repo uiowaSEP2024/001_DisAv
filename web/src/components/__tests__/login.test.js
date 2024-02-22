8225b59125fdd2199dd89a700e6ed769faad866f
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2024-02-21 20:51:39 -0600

    trying to up code coverage of login

--- web/src/components/__tests__/login.test.js
+++ web/src/components/__tests__/login.test.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import axios from 'axios';
-import Login from '../Login';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
+import App from '../../App';
+import { AuthContext } from '../AuthContext';
 
-jest.mock('axios');
+test('renders App component with Navbar', () => {
+  const mockAuthContextValue = {
+    isLoggedIn: true,
+    logout: jest.fn(),
+  };
 
-describe('Login', () => {
-  it('renders the login form', () => {
-    render(
-      <MemoryRouter>
-        <Login />
+  render(
+    <AuthContext.Provider value={mockAuthContextValue}>
+      <MemoryRouter> {/* Wrap the App component with MemoryRouter */}
+        <App />
       </MemoryRouter>
-    );
-    const form = screen.getByTestId('login-form');
-    expect(form).toBeInTheDocument();
-  });
+    </AuthContext.Provider>
+  );
 
-  it('calls axios post on form submission', async () => {
-    render(
-      <MemoryRouter>
-        <Login />
+  const navbarElement = screen.getByTestId('navbar'); // Ensure the case matches the actual text
+  expect(navbarElement).toBeInTheDocument();
+});
+
+test('logs out user when logout button is clicked', () => {
+  const mockAuthContextValue = {
+    isLoggedIn: true,
+    logout: jest.fn(),
+  };
+
+  render(
+    <AuthContext.Provider value={mockAuthContextValue}>
+      <MemoryRouter> {/* Wrap the App component with MemoryRouter */}
+        <App />
       </MemoryRouter>
-    );
+    </AuthContext.Provider>
+  );
 
-    const usernameInput = screen.getByLabelText('Username:');
-    const passwordInput = screen.getByLabelText('Password:');
+  const logoutButton = screen.getByText('Log Out');
+  fireEvent.click(logoutButton);
 
-    fireEvent.change(usernameInput, { target: { value: 'adnane' } });
-    fireEvent.change(passwordInput, { target: { value: '1234' } });
-
-    axios.post.mockResolvedValue({ data: {} });
-
-    fireEvent.submit(screen.getByTestId('login-form'));
-
-    await waitFor(() => expect(axios.post).toHaveBeenCalled());
-  });
+  expect(mockAuthContextValue.logout).toHaveBeenCalled();
 });
