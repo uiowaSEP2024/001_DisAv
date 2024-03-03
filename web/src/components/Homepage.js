a43c6e7e30b752dfa2dffa3855a4894c13bdb025
Joslin Some <joslin-some@uiowa.edu>
2024-03-03 15:08:08 -0600

    Fixing issues

--- web/src/components/Homepage.js
+++ web/src/components/Homepage.js
@@ -3,8 +3,6 @@ import '../styles/homepage.css';
 // import Navbar from './Navbar';
 
 const Homepage = () => {
-  axios.get('http://localhost:3002/task/get-by-username', {username: "JoslinSome"})
-    .then(r=>console.log("Hello"))
   return (
     <div className="homepage" data-testid="homepage">
       {/* Rest of the homepage content */}
